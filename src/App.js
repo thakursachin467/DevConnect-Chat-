d6791a65f98e0221ce563a9fc20d603c6183c94b
thakursachin467 <thakursachin467@gmail.com>
2018-10-07 22:29:04 +0530

    mockup added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
-
+import Message from './Components/Message';
+import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
-      </div>
+
+      <Message />
+
     );
   }
 }
