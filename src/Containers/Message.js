0ef8a10a28ff58388f126c60f5370f5cfbc69517
thakursachin467 <thakursachin467@gmail.com>
2018-10-13 23:44:10 +0530

    ui changes to the online user sidebar

--- src/Containers/Message.js
+++ src/Containers/Message.js
@@ -39,13 +39,14 @@ const Message = () => {
           </div>
 
         </li>
-        <li>
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  <strong>Jane Doe</strong>
+        <li className='Current-User'>
+          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" /> <strong>Sachin Thakur</strong>
           <div className='message-body'>
-            <span className='message-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tellus velit. Quisque id tempus lacus. Aliquam augue sapien, placerat ac sagittis ut, ultricies at quam. Mauris mauris diam, eleifend id convallis vitae, vulputate non ipsum. Donec quis quam non sem feugiat scelerisque nec a quam. Sed semper, ex nec placerat rhoncus, metus eros tempus sem, et varius mi libero nec turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean varius orci non viverra sollicitudin. Sed malesuada massa ut dui volutpat, non lacinia mauris commodo. Curabitur erat risus, consectetur ornare ex elementum, cursus ullamcorper est.</span>
+            <span className='message-text '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tellus velit. Quisque id tempus lacus. Aliquam augue sapien, placerat ac sagittis ut, ultricies at quam. Mauris mauris diam, eleifend id convallis vitae, vulputate non ipsum. Donec quis quam non sem feugiat scelerisque nec a quam. Sed semper, ex nec placerat rhoncus, metus eros tempus sem, et varius mi libero nec turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean varius orci non viverra sollicitudin. Sed malesuada massa ut dui volutpat, non lacinia mauris commodo. Curabitur erat risus, consectetur ornare ex elementum, cursus ullamcorper est.</span>
           </div>
 
         </li>
+
 
       </ul>
     </div>
--- src/Containers/Users.js
+++ src/Containers/Users.js
@@ -7,53 +7,139 @@ const Users = () => {
         <span className='online-user-text'> Online Users</span>
       </div>
       <ul className='online-list'>
+        <li className='online-user-list'>
+
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  John Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">John Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  jane Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">Jane Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  yes Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">Ok Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  no Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">No Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ok Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">Yes Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">YUp Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">Angry Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">Angry Doe</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">NOPE dOE</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">ok</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">idk</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">les</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
         <li className="online-user-list">
-          <img class="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />  ko Doe last
-      </li>
+          <img className="avatar-online" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+          <div class="about">
+            <div class="name">no</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
+        </li>
       </ul>
       <div style={{ height: '4rem' }}>
 
       </div>
       <div className="inner">
-        <img class="avatar" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
-        <div class="channels-footer-details">
-          <span class="username">Sachin Thakur</span>
+        <img className="avatar" alt="Avatar" src="https://us.123rf.com/450wm/triken/triken1608/triken160800029/61320775-stock-vector-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg?ver=6" />
+        <div className="channels-footer-details">
+          <div class="about">
+            <div class="name">Sachin Thakur</div>
+            <div class="status">
+              <span className='dot online'></span> online
+            </div>
+          </div>
         </div>
       </div>
 
