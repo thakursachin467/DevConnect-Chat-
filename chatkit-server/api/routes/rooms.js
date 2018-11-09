5c3d0d289605e8f46450c64116e4954f8583b636
thakursachin467 <thakursachin467@gmail.com>
2018-11-09 21:46:50 +0530

    api added to handle add user to room and delete room

--- chatkit-server/api/routes/rooms.js
+++ chatkit-server/api/routes/rooms.js
@@ -4,11 +4,12 @@ const User = require('../../models/User');
 const chatkit = require('../../Config/chatkit');
 
 
-//@route POST /api/room/createRoom/:name
+//@route POST /api/room/invite/:name
 //@description create a room
 //@access private route
 
-router.get('/createRoom/:name', (req, res) => {
+router.get('/invite/:teamId', (req, res) => {
+
 
 });
 
@@ -17,8 +18,46 @@ router.get('/createRoom/:name', (req, res) => {
 //@description delete a room
 //@access private route
 
-router.get('/deleteRoom/:id', (req, res) => {
+router.get('/add/:id', (req, res) => {
+  const userIds = req.body.user;
+  chatkit.addUsersToRoom({
+    roomId: room.id,
+    userIds: userIds
+  })
+    .then(() => {
+      console.log('added');
+      res.status(200).json({
+        sucess: true
+      })
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(400).json({
+        sucess: false,
+        error: err
+      })
+    })
+});
 
+
+router.get('/delete/:id', (req, res) => {
+  const roomId = req.body.roomId;
+  chatkit.deleteRoom({
+    id: Number(roomId)
+  })
+    .then(() => {
+      res.status(200).json({
+        sucess: true
+      })
+      console.log('gone forever')
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).json({
+        sucess: false,
+        error: err
+      })
+    })
 });
 
 
