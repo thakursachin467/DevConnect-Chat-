f0f9621162aa8652fcd016d0a1afbfbdf45a7f0c
Sachin Thakur <thakursachin467@gmail.com>
2019-03-19 00:37:27 +0530

    Kubernetes configs added

--- chatkit-server/Config/chatkit.js
+++
@@ -1,7 +0,0 @@
-const Chatkit = require('@pusher/chatkit-server');
-const chatkit = new Chatkit.default({
-  instanceLocator: process.env.INSTANCE_CHATKIT,
-  key: process.env.CHATKIT_KEY,
-});
-
-module.exports = chatkit;
--- chatkit-server/Config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://admin:admin123@ds237770.mlab.com:37770/chatkit-demo",
-  secretOrKey: "YOURSECRET"
-}
